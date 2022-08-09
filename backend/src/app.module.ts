b6fc4024b97b175b7ea5a730a36ba6fdb0e399f1
Edward <wardvisual@gmail.com>
2022-08-09 20:30:55 +0800

    chore: configure module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,15 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { UserModule } from './user/user.module';
-import { PrismaModule } from './prisma/prisma.module';
-import { PrismaModule } from './prisma/prisma.module';
-import { UserModule } from './user/user.module';
+import { Module } from '@nestjs/common';
+
+import { PrismaModule } from '@/prisma/prisma.module';
+import { UserModule } from '@/user/user.module';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), UserModule, PrismaModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
