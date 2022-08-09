5e6d9910e250f2725de94ea6fe5cd7bb5b70bd4a
Edward <wardvisual@gmail.com>
2022-08-09 20:28:00 +0800

    refactor: add database config

---
+++ backend/database/user.ts
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+export const users = [
+  {
+    name: 'ward',
+    email: 'aw@ds.com',
+  },
+  {
+    name: 'cool',
+    email: 'afdd@ds.com',
+  },
+  {
+    name: 'lee',
+    email: 'edwa@ds.com',
+  },
+];
