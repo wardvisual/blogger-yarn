a22493943c5a54cd2b1b87cd8ba1cf8b7cdab016
Edward <wardvisual@gmail.com>
2022-08-09 20:28:14 +0800

    chore: remove cntroller

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './user/user.service';
-import { UserController } from './user/user.controller';
+import { UserService } from '@/user/service/user.service';
+import { UserController } from '@/user/controller/user.controller';
+import { PrismaModule } from '@/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [UserService],
-  controllers: [UserController]
+  controllers: [UserController],
+  exports: [UserService],
 })
 export class UserModule {}
