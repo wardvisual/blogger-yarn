60b44db0d1a123e644cd94c44bf0d02ebbb6a1ce
Edward <wardvisual@gmail.com>
2022-08-10 22:34:41 +0800

    chore: update migrations

---
+++ backend/database/migrations/20220810142413_test/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE `Wow` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `usernmae` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `haha` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `usernmae` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
