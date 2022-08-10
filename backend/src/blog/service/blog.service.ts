e411bb3e5ee9056b58151240e573a732b5114fc6
Edward <wardvisual@gmail.com>
2022-08-10 22:35:56 +0800

    chore: update blog service

--- backend/src/blog/service/blog.service.ts
+++ backend/src/blog/service/blog.service.ts
@@ -3,5 +3,7 @@ import { PrismaService } from '@/prisma/service/prisma.service';
 
 @Injectable()
 export class BlogService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(private readonly prismaService: PrismaService) {
+    prismaService.blogger;
+  }
 }
