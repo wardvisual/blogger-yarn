aa810a77a7a6ad7c64312549665700b3148cf2e2
Edward <wardvisual@gmail.com>
2022-08-10 22:35:19 +0800

    chore: update import

--- backend/src/prisma/prisma.module.ts
+++ backend/src/prisma/prisma.module.ts
@@ -1,5 +1,5 @@
 import { Module, Global } from '@nestjs/common';
-import { PrismaService } from '@/prisma/service/prisma.service';
+import { PrismaService } from '@/prisma/prisma.service';
 
 @Global()
 @Module({
