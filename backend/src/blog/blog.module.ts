61ddd66c05e0078ee5cd8158028376d10891caef
Edward <wardvisual@gmail.com>
2022-08-11 21:05:13 +0800

    chore: delete unecessary files

--- backend/src/blog/blog.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BlogController } from '@/blog/controller/blog.controller';
-import { BlogService } from '@/blog/service/blog.service';
-
-@Module({
-  controllers: [BlogController],
-  providers: [BlogService],
-})
-export class BlogModule {}
--- backend/src/blog/service/blog.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from '@/prisma/service/prisma.service';
-
-@Injectable()
-export class BlogService {
-  constructor(private readonly prismaService: PrismaService) {
-    prismaService.blogger;
-  }
-}
