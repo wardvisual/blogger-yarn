419ac56ddb3e9e99dfe14d26aafe3f170bbe66da
Edward <wardvisual@gmail.com>
2022-08-12 20:52:15 +0800

    chore:add global prefix named 'api'

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('v1/api');
+
   await app.listen(3000);
 }
 bootstrap();
