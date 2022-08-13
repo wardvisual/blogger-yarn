9f6b1a4bf269baf838f3acaa7ee8d902b4436ddb
Edward <wardvisual@gmail.com>
2022-08-13 20:59:58 +0800

    chore: start to implement service testing

--- backend/src/user/service/user.service.spec.ts
+++ backend/src/user/service/user.service.spec.ts
@@ -1,13 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { UserService } from './user.service';
+import { UserService } from '@/user/service/user.service';
+import { PrismaService } from '@/prisma/prisma.service';
+import { UserController } from '../controller/user.controller';
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      controllers: [UserController],
+      imports: [],
       providers: [UserService],
-    }).compile();
+    })
+      .overrideProvider(UserService)
+      .useValue({})
+      .compile();
 
     service = module.get<UserService>(UserService);
   });
