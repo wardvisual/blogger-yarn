abfe0dea013052979da6fe9378d81b515d3ce7ad
Edward <wardvisual@gmail.com>
2022-08-13 21:00:15 +0800

    chore: start to implement controllere testing

--- backend/src/user/controller/user.controller.spec.ts
+++ backend/src/user/controller/user.controller.spec.ts
@@ -1,18 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { UserService } from '../service/user.service';
 
 describe('UserController', () => {
   let controller: UserController;
 
+  const userMockService = {
+    create: jest.fn((dto) => {
+      return { id: Date.now(), ...dto };
+    }),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
-    }).compile();
+      imports: [],
+      providers: [UserService],
+    })
+      .overrideProvider(UserService)
+      .useValue(userMockService)
+      .compile();
 
     controller = module.get<UserController>(UserController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should create a user', () => {
+    expect(controller.create({ username: 'ward', password: 'pass' })).toEqual({
+      id: expect.any(Number),
+      username: 'ward',
+      password: 'pass',
+    });
   });
 });
