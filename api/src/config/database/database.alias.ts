f8e1d4479e875e99db6d68f3cd30caded8e063b5
Edward <wardvisual@gmail.com>
2022-08-17 17:20:50 +0800

    chore: mini changes

--- api/src/config/database/database.alias.ts
+++
@@ -1,9 +0,0 @@
-import { registerAs } from '@nestjs/config';
-
-const databaseConfigAlias = registerAs('db', () => ({
-  name: process.env.DATABASE_NAME,
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-}));
-
-export default databaseConfigAlias;
