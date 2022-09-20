ebaf67571fcd5217fd4671e8976b2a66614ca90d
wardvisual <wardvisual@gmail.com>
2022-09-20 15:20:13 +0000

    chore: create post entity

--- api/src/models/post/entities/post.entity.ts
+++ api/src/models/post/entities/post.entity.ts
@@ -1,1 +1,25 @@
-export class Post {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'posts' })
+export class Post {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column({ unique: true })
+  slug: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
+
+  @Column()
+  imageUrl: string;
+}
