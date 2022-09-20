d8108af0eaaa37e9f59c601f3d9071b43413430f
wardvisual <wardvisual@gmail.com>
2022-09-20 15:19:39 +0000

    chore: update post module metadata

--- api/src/models/post/post.module.ts
+++ api/src/models/post/post.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import { PostService } from './post.service';
+
 import { PostController } from './post.controller';
+import { Post } from './entities/post.entity';
+import { PostService } from './post.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   controllers: [PostController],
-  providers: [PostService]
+  providers: [PostService],
+  imports: [TypeOrmModule.forFeature([Post])],
+  exports: [TypeOrmModule],
 })
 export class PostModule {}
