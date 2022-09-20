ff252627a9a31d208d7a5e34a2ab683ef40c4aeb
wardvisual <wardvisual@gmail.com>
2022-09-20 19:18:44 +0000

    chore: implement exceptions filter

---
+++ api/src/common/exceptions/all-exceptions.filter.ts
@@ -0,0 +1,31 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
+import { HttpStatus } from '@nestjs/common';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
+
+  catch(exception: unknown, host: ArgumentsHost): void {
+    const { httpAdapter } = this.httpAdapterHost;
+
+    const ctx = host.switchToHttp();
+
+    const httpStatus =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const responseBody = Object.assign(exception, {
+      timestamp: new Date().toISOString(),
+      path: httpAdapter.getRequestUrl(ctx.getRequest()),
+    });
+
+    httpAdapter.reply(ctx.getResponse(), responseBody, httpStatus);
+  }
+}
