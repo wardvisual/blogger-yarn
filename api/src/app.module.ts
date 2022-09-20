aac39aa602f1adaf57ce27aefe1735acbad74f7c
wardvisual <wardvisual@gmail.com>
2022-09-20 19:20:35 +0000

    chore: import the exeception filter to providers

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER } from '@nestjs/core';
 
 import { DatabaseProviderModule } from '@/providers/database/mysql/provider.module';
+import { AllExceptionsFilter } from '@/common/exceptions/all-exceptions.filter';
 
 @Module({
   imports: [
@@ -11,6 +13,9 @@ import { DatabaseProviderModule } from '@/providers/database/mysql/provider.modu
     DatabaseProviderModule,
   ],
   controllers: [],
-  providers: [DatabaseProviderModule],
+  providers: [
+    DatabaseProviderModule,
+    { provide: APP_FILTER, useClass: AllExceptionsFilter },
+  ],
 })
 export class AppModule {}
