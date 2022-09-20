77242527c76f483e52a25aff097da5f3e2e997e0
wardvisual <wardvisual@gmail.com>
2022-09-20 20:46:40 +0000

    chore: document create-post-dto to swagger api

--- api/src/models/post/dto/create-post.dto.ts
+++ api/src/models/post/dto/create-post.dto.ts
@@ -1,1 +1,12 @@
-export class CreatePostDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreatePostDto {
+  @ApiProperty()
+  title: string;
+
+  @ApiProperty()
+  description: string;
+
+  @ApiProperty()
+  imageUrl: string;
+}
