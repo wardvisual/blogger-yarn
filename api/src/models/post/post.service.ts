186a936682fd8079ae916bf908aa381e6a73dfe7
wardvisual <wardvisual@gmail.com>
2022-09-20 15:18:34 +0000

    chore: inject database layer

--- api/src/models/post/post.service.ts
+++ api/src/models/post/post.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { Post } from './entities/post.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class PostService {
+  constructor(
+    @InjectRepository(Post) private readonly repository: Repository<Post>,
+  ) {}
+
   create(createPostDto: CreatePostDto) {
     return 'This action adds a new post';
   }
 
   findAll() {
-    return `This action returns all post`;
+    return this.repository.find();
   }
 
   findOne(id: number) {
