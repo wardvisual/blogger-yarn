c24b6a94a6560bb21ca7d3d8247604371fc0825d
wardvisual <wardvisual@gmail.com>
2022-09-20 22:20:12 +0000

    chore: document update-post-dto to swagger

--- api/src/models/post/dto/update-post.dto.ts
+++ api/src/models/post/dto/update-post.dto.ts
@@ -1,4 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePostDto } from './create-post.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdatePostDto extends PartialType(CreatePostDto) {}
+export class UpdatePostDto extends PartialType(CreatePostDto) {
+  @ApiProperty({ required: false })
+  title?: string;
+
+  @ApiProperty({ required: false })
+  description?: string;
+}
