80aff797bdd38b87775ff262fd72557cb580a827
wardvisual <wardvisual@gmail.com>
2022-09-22 19:58:17 +0000

    chore: bind 'ValidationPipe' at the application level

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,4 +1,4 @@
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
@@ -7,10 +7,14 @@ import { AppModule } from '@/app.module';
 async function bootstrap() {
   const app: INestApplication = await NestFactory.create(AppModule);
 
+  app.useGlobalPipes(new ValidationPipe());
+
   /* Swagger */
   const config = new DocumentBuilder()
     .setTitle('Blog API')
     .setVersion('1.0.0')
+    .setDescription('An API for Blog Application')
+    .setContact('Edward Fernandez', 'https://www.wardvisual.me/', '')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
