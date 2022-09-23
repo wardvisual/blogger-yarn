6dfeb78a51ea7f344159bf3decb2b8d0771b8ef0
wardvisual <wardvisual@gmail.com>
2022-09-23 01:17:49 +0000

    fix(branch): recover fix/sql-relations branch

--- api/src/models/category/entities/category.entity.ts
+++ api/src/models/category/entities/category.entity.ts
@@ -1,20 +1,29 @@
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
-import { PostEntity } from '../../post/entities/post.entity';
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
+import { PostEntity } from '@/models/post/entities/post.entity';
 
 @Entity({ name: 'categories' })
 export class CategoryEntity {
   @PrimaryGeneratedColumn()
   id: string;
 
+  @IsNotEmpty()
+  @IsString()
   @Column()
   title: string;
 
+  @IsNotEmpty()
+  @IsString()
   @Column()
   description: string;
 
+  @IsNotEmpty()
+  @IsDate()
   @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   createdAt: Date;
 
+  @IsNotEmpty()
+  @IsDate()
   @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   updatedAt: Date;
 
--- api/src/models/user/entities/user.entity.ts
+++ api/src/models/user/entities/user.entity.ts
@@ -8,29 +8,42 @@ import {
   PrimaryGeneratedColumn,
 } from 'typeorm';
 
+import { IsNotEmpty, MinLength, IsEmail, IsString } from 'class-validator';
 @Entity({ name: 'users' })
 export class UserEntity {
   @ApiProperty()
   @PrimaryGeneratedColumn()
-  id: string;
+  id: number;
 
   @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
   @Column()
   firstname: string;
 
   @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
   @Column()
   lastname: string;
 
   @ApiProperty()
+  @IsNotEmpty()
+  @IsEmail()
   @Column()
   email: string;
 
   @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
   @Column()
   password: string;
 
   @ApiProperty()
+  @IsString()
   @Column()
   avatar: string;
 
