20009ae19e8340a516c9b107d7facfdc1c8598df
wardvisual <wardvisual@gmail.com>
2022-09-23 18:55:59 +0000

    chore(resource/post): update DTo validation properties

--- api/src/models/post/dto/update-post.dto.ts
+++ api/src/models/post/dto/update-post.dto.ts
@@ -1,20 +1,23 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePostDto } from './create-post.dto';
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, MinLength } from 'class-validator';
+import { IsOptional, IsString, MinLength } from 'class-validator';
 
 export class UpdatePostDto extends PartialType(CreatePostDto) {
   @ApiProperty({ required: false })
   @IsString()
   @MinLength(6)
+  @IsOptional()
   title?: string;
 
   @ApiProperty({ required: false })
   @IsString()
   @MinLength(40)
+  @IsOptional()
   description?: string;
 
   @ApiProperty({ required: false })
   @IsString()
+  @IsOptional()
   imageUrl?: string;
 }
