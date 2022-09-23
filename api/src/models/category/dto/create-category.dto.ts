056b09ec1411aced6f7ae17ca025aaae442c0014
wardvisual <wardvisual@gmail.com>
2022-09-23 18:22:49 +0000

    chore(models/dto): validate category DTOs

--- api/src/models/category/dto/create-category.dto.ts
+++ api/src/models/category/dto/create-category.dto.ts
@@ -1,9 +1,15 @@
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateCategoryDto {
   @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(2)
   title: string;
 
   @ApiProperty({ required: false })
+  @IsString()
+  @MinLength(10)
   description?: string;
 }
--- api/src/models/category/dto/update-category.dto.ts
+++ api/src/models/category/dto/update-category.dto.ts
@@ -1,11 +1,16 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { ApiProperty } from '@nestjs/swagger';
 import { CreateCategoryDto } from './create-category.dto';
+import { IsString, MinLength } from 'class-validator';
 
 export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {
-  @ApiProperty({ required: false })
+  @ApiProperty()
+  @IsString()
+  @MinLength(2)
   title?: string;
 
   @ApiProperty({ required: false })
+  @IsString()
+  @MinLength(10)
   description?: string;
 }
