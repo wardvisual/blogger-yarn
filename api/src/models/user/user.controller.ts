ad9c04d7f085f44fa585b501d79c39876f35967c
wardvisual <wardvisual@gmail.com>
2022-09-25 03:09:20 +0000

    chore(user-controller): transform method to async

--- api/src/models/user/user.controller.ts
+++ api/src/models/user/user.controller.ts
@@ -18,8 +18,8 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+  async create(@Body() createUserDto: CreateUserDto) {
+    return await this.userService.create(createUserDto);
   }
 
   @Get()
