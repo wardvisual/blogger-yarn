f1afcb94c72ff07dd8c9f2475e0732c3de37fcc5
wardvisual <wardvisual@gmail.com>
2022-09-28 18:35:36 +0000

    refactor(dto/update): remove unecessary decorator

--- api/src/models/post/dto/update-post.dto.ts
+++ api/src/models/post/dto/update-post.dto.ts
@@ -1,23 +1,17 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePostDto } from './create-post.dto';
 import { ApiProperty } from '@nestjs/swagger';
-import { IsOptional, IsString, MinLength } from 'class-validator';
 
 export class UpdatePostDto extends PartialType(CreatePostDto) {
   @ApiProperty({ required: false })
-  @IsString()
-  @MinLength(6)
-  @IsOptional()
-  title?: string;
+  title: string;
 
   @ApiProperty({ required: false })
-  @IsString()
-  @MinLength(40)
-  @IsOptional()
-  description?: string;
+  description: string;
 
   @ApiProperty({ required: false })
-  @IsString()
-  @IsOptional()
-  imageUrl?: string;
+  imageUrl: string;
+
+  @ApiProperty({ required: false })
+  categoryId: number;
 }
