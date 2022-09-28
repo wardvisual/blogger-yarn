86c837a9017cc982a185cbe3eda4660002a3d599
wardvisual <wardvisual@gmail.com>
2022-09-28 20:20:20 +0000

    refactor(dto/category): remove unecessary validation

--- api/src/models/category/dto/update-category.dto.ts
+++ api/src/models/category/dto/update-category.dto.ts
@@ -1,17 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { ApiProperty } from '@nestjs/swagger';
 import { CreateCategoryDto } from './create-category.dto';
-import { IsOptional, IsString, MinLength } from 'class-validator';
 
 export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {
   @ApiProperty()
-  @IsString()
-  @MinLength(2)
-  title?: string;
+  title: string;
 
   @ApiProperty({ required: false })
-  @IsString()
-  @IsOptional()
-  @MinLength(10)
-  description?: string;
+  description: string;
 }
