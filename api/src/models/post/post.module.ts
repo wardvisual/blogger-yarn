467510f9fa6a86ff6ca0340b0ec71e69d96e402a
wardvisual <wardvisual@gmail.com>
2022-09-28 18:34:24 +0000

    chore(post-service): import category entity

--- api/src/models/post/post.module.ts
+++ api/src/models/post/post.module.ts
@@ -4,11 +4,12 @@ import { PostController } from './post.controller';
 import { PostEntity } from './entities/post.entity';
 import { PostService } from './post.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoryEntity } from '../category/entities/category.entity';
 
 @Module({
   controllers: [PostController],
   providers: [PostService],
-  imports: [TypeOrmModule.forFeature([PostEntity])],
+  imports: [TypeOrmModule.forFeature([PostEntity, CategoryEntity])],
   exports: [TypeOrmModule],
 })
 export class PostModule {}
