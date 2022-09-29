801e5382867c7ca35af60c65e8caedafce932512
wardvisual <wardvisual@gmail.com>
2022-09-29 17:32:44 +0000

    chore(dto/create-user): validate create-user-dto

--- api/src/models/user/dto/create-user.dto.ts
+++ api/src/models/user/dto/create-user.dto.ts
@@ -1,18 +1,29 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
   @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(2)
   firstname: string;
 
   @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(2)
   lastname: string;
 
   @ApiProperty()
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
 
   @ApiProperty()
+  @IsNotEmpty()
   password: string;
 
   @ApiProperty({ required: false })
+  @IsString()
   avatar?: string;
 }
