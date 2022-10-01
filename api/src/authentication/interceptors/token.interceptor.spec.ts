99587cede16b9181b18aa19c137093ff4d5bf387
wardvisual <wardvisual@gmail.com>
2022-10-01 10:28:41 +0000

    chore(auth/interceptor): create interceptor for http response

---
+++ api/src/authentication/interceptors/token.interceptor.spec.ts
@@ -0,0 +1,7 @@
+import { TokenInterceptor } from './token.interceptor';
+
+describe('TokenInterceptor', () => {
+  it('should be defined', () => {
+    expect(new TokenInterceptor()).toBeDefined();
+  });
+});
---
+++ api/src/authentication/interceptors/token.interceptor.ts
@@ -0,0 +1,41 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+import { AuthService } from '../auth.service';
+import { UserEntity } from '../../models/user/entities/user.entity';
+import { ConfigService } from '../../config/database/mysql/config.service';
+import { Response } from 'express';
+
+@Injectable()
+export class TokenInterceptor implements NestInterceptor {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<UserEntity> {
+    return next.handle().pipe(
+      map((user) => {
+        const response = context.switchToHttp().getResponse<Response>();
+        const token = this.authService.signToken(user);
+
+        response.setHeader('Authorization', `Bearer ${token}`);
+        response.cookie('token', token, {
+          httpOnly: this.configService.get('NODE_ENV') === 'development',
+          signed: true,
+          sameSite: 'strict',
+          secure: this.configService.get('NODE_ENV') === 'development',
+        });
+
+        return user;
+      }),
+    );
+  }
+}
