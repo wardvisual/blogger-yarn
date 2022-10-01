cd1282181de58591886189c4a9b95e353df38d18
wardvisual <wardvisual@gmail.com>
2022-10-01 10:37:43 +0000

    chore(auth/interceptors): import config service from @nest/config

--- api/src/authentication/interceptors/token.interceptor.ts
+++ api/src/authentication/interceptors/token.interceptor.ts
@@ -6,15 +6,15 @@ import {
 } from '@nestjs/common';
 import { map, Observable } from 'rxjs';
 import { AuthService } from '../auth.service';
-import { UserEntity } from '../../models/user/entities/user.entity';
-import { ConfigService } from '../../config/database/mysql/config.service';
+import { UserEntity } from '@/models/user/entities/user.entity';
 import { Response } from 'express';
+import { ConfigService as NestConfigService } from '@nestjs/config';
 
 @Injectable()
 export class TokenInterceptor implements NestInterceptor {
   constructor(
     private readonly authService: AuthService,
-    private readonly configService: ConfigService,
+    private readonly configService: NestConfigService,
   ) {}
 
   intercept(
