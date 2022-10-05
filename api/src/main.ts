9832d5a08fd255ed706a97711a7770839b6823bb
wardvisual <wardvisual@gmail.com>
2022-10-05 12:19:03 +0000

    chore: implement global variable for session and middleware

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,13 +1,32 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-
 import { AppModule } from '@/app.module';
+import * as cookieParser from 'cookie-parser';
+import * as passport from 'passport';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app: INestApplication = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+  app.use(cookieParser(process.env.APP_SECRET));
+  app.use(
+    session({
+      secret: 'my-secret',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  // app.enableCors({
+  //   origin: process.env.ALLOWED_ORIGINS?.split(/\s*,\s*/) ?? '*',
+  //   credentials: true,
+  //   exposedHeaders: ['Authorization'],
+  // });
 
   /* Swagger */
   const config = new DocumentBuilder()
@@ -21,8 +40,7 @@ async function bootstrap() {
 
   SwaggerModule.setup('api', app, document);
 
-  /* Listening to PORT 3000.. */
-  await app.listen(3000);
+  await app.listen(process.env.APP_PORT);
 }
 
 bootstrap();
