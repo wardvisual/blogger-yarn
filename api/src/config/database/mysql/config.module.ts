e4437d38f4942405c206fd7daf03bef380a19c5a
wardvisual <wardvisual@gmail.com>
2022-10-05 12:21:05 +0000

    chore(config/db): update imports

--- api/src/config/database/mysql/config.module.ts
+++ api/src/config/database/mysql/config.module.ts
@@ -3,10 +3,11 @@ import { ConfigService } from '@/config/database/mysql/config.service';
 import { PostModule } from '@/models/post/post.module';
 import { CategoryModule } from '@/models/category/category.module';
 import { UserModule } from '@/models/user/user.module';
+import { AuthModule } from '@/authentication/auth.module';
 
 @Module({
   providers: [ConfigService],
   exports: [ConfigService],
-  imports: [UserModule, PostModule, CategoryModule],
+  imports: [UserModule, PostModule, CategoryModule, AuthModule],
 })
 export class ConfigModule {}
