7914026a45b6628ee047ba082a43a24596e3a445
Edward <wardvisual@gmail.com>
2022-10-25 07:49:16 +0000

    chore: implement interactivity

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,14 +7,21 @@ import { HomeComponent } from './views/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
+import { CoreModule } from './core/core.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, NavbarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    CoreModule,
+    HttpClientModule,
     BrowserAnimationsModule,
     MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
