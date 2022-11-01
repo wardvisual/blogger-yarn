4bbd8ee09d5041bf4c24908530578dea0ef37aa6
Edward <wardvisual@gmail.com>
2022-11-01 19:25:59 +0000

    chore: update blog component

--- client/src/app/views/blog/create/create.component.ts
+++ client/src/app/views/blog/create/create.component.ts
@@ -1,12 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
+interface Creatable {
+  title: string;
+  description: string;
+  imageUrl: string;
+}
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.scss'],
 })
 export class Create implements OnInit {
-  constructor() {}
+  constructor(private formBuilder: FormBuilder) {}
 
+  public createForm = this.formBuilder.group<Creatable>({
+    title: '',
+    description: '',
+    imageUrl: '',
+  });
+
+  public submitForm(): void {
+    console.log({ value: this.createForm.value });
+  }
   ngOnInit(): void {}
 }
