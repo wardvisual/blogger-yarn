88fd5abc39d0fbf798e3af5ef24e84141ba51cbf
Edward <wardvisual@gmail.com>
2022-11-02 20:41:54 +0000

    chore: add elements for create-component

--- client/src/app/views/blog/create/create.component.ts
+++ client/src/app/views/blog/create/create.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { discardPeriodicTasks } from '@angular/core/testing';
 import { FormBuilder } from '@angular/forms';
+import { disableDebugTools, DomSanitizer } from '@angular/platform-browser';
 
 interface Creatable {
   title: string;
@@ -14,14 +16,34 @@ interface Creatable {
 export class Create implements OnInit {
   constructor(private formBuilder: FormBuilder) {}
 
+  imageCover: any = '';
+  coverImageErrorMsg: string = '';
+
   public createForm = this.formBuilder.group<Creatable>({
     title: '',
     description: '',
-    imageUrl: '',
+    imageUrl: this.imageCover,
   });
 
   public submitForm(): void {
     console.log({ value: this.createForm.value });
+  }
+
+  public onImageCoverSelected(event: any): void {
+    const file = event.target.files[0].name;
+    const mimeType = event.target.files[0].type;
+
+    if (mimeType.match(/image\/*/) == null) {
+      this.coverImageErrorMsg = 'Only image is supported';
+    }
+
+    const reader = new FileReader();
+    reader.readAsDataURL(event.target.files[0]);
+
+    reader.onload = (_event) => {
+      this.imageCover = reader.result;
+      console.log({ _event, reader });
+    };
   }
 
   ngOnInit(): void {}
