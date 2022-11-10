5441c88cd449c099db29d466f009f6a10fe346a4
Edward <wardvisual@gmail.com>
2022-11-10 15:39:54 +0000

    chore: create button component

---
+++ client/src/app/components/button/button.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+
+enum ButtonType {
+  PRIMARY,
+  SECONDARY,
+  OUTLINE,
+}
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss'],
+})
+export class ButtonComponent implements OnInit {
+  @Input() name: String = '';
+  @Input() type: String = '';
+  @Output() onClick = new EventEmitter();
+
+  constructor() {}
+
+  public emitEvent() {
+    this.onClick.emit();
+  }
+
+  ngOnInit(): void {}
+}
