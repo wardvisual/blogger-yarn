3a2d48911528dc0830c6f9d3c95fc439c90678a0
Edward <wardvisual@gmail.com>
2022-11-10 15:40:16 +0000

    chore: import button component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { Views } from './views';
 /* Componetns */
 import { Component } from './components';
 import { MarkdownModule } from 'ngx-markdown';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { MarkdownModule } from 'ngx-markdown';
     Component.NavbarComponent,
     Component.CardComponent,
     Component.IconComponent,
+    ButtonComponent,
   ],
   imports: [
     BrowserModule,
