70f4047af41f06c48719545982350758133387b4
Edward <wardvisual@gmail.com>
2022-11-20 08:59:26 +0000

    chore: omit blog view component

--- client/src/app/views/index.ts
+++ client/src/app/views/index.ts
@@ -1,9 +1,7 @@
-import * as BlogComponent from './blog/index';
 import * as HomeComponent from './home/home.component';
 import * as CreateBlogComponent from './create-blog/create-blog.component';
 
 export const Views = {
-  ...BlogComponent,
   ...HomeComponent,
   ...CreateBlogComponent,
 };
