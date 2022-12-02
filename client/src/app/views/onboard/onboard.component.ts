976a82164270dfebd803c3978330825da7b066de
Edward <wardvisual@gmail.com>
2022-12-02 16:24:02 +0000

    chore: add onboard page for login and register view

---
+++ client/src/app/views/onboard/onboard.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-onboard',
+  templateUrl: './onboard.component.html',
+  styleUrls: ['./onboard.component.scss'],
+})
+export class OnboardComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
