d27d46bab8f922db81a0d074bb44cd82e3b2fef2
Edward <wardvisual@gmail.com>
2022-12-02 20:53:45 +0000

    chore: add markdown component

---
+++ client/src/app/components/markdown/markdown.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import '@github/markdown-toolbar-element';
+
+@Component({
+  selector: 'app-markdown',
+  templateUrl: './markdown.component.html',
+  styleUrls: ['./markdown.component.scss'],
+})
+export class MarkdownComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
