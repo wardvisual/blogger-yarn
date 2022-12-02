8abb718c5f064cd1b44da63f4c5ac493735a3ff8
Edward <wardvisual@gmail.com>
2022-12-02 16:24:17 +0000

    chore: refactor and update components

--- client/src/app/views/index.ts
+++ client/src/app/views/index.ts
@@ -1,7 +1,9 @@
 import * as HomeComponent from './home/home.component';
 import * as CreateBlogComponent from './create-blog/create-blog.component';
+import * as OnboardComponent from './onboard/onboard.component';
 
 export const Views = {
   ...HomeComponent,
   ...CreateBlogComponent,
+  ...OnboardComponent,
 };
