9d50ee1cca5196e7d3e73c0678716ff124b67379
Edward <wardvisual@gmail.com>
2022-12-04 18:45:02 +0000

    chore: update markdown component

--- client/src/app/components/markdown/markdown.component.ts
+++ client/src/app/components/markdown/markdown.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormControl, FormBuilder, FormGroup } from '@angular/forms';
 import '@github/markdown-toolbar-element';
+import { marked } from 'marked';
 
 @Component({
   selector: 'app-markdown',
@@ -7,7 +9,16 @@ import '@github/markdown-toolbar-element';
   styleUrls: ['./markdown.component.scss'],
 })
 export class MarkdownComponent implements OnInit {
+  @Input() description: string = '';
+  @Output() event!: EventEmitter<string>;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.event = new EventEmitter<string>();
+  }
+
+  sendData(): void {
+    this.event.emit(marked(this.description));
+  }
 }
